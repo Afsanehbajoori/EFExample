2302edbd70c958da432e62847106d1c07b9c8317
Afsaneh Bajoori <afsan.bajoori@hotmail.com>
2021-10-27 14:49:25 +0200

    update data in the table

--- EFExample/Program.cs
+++ EFExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EFExample.Data_Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EFExample
 {
@@ -10,81 +11,66 @@ namespace EFExample
         {
             using Context myContext = new Context();
 
-            /*myContext.Class.Add(new Class()
-            {
-                Name = "EnglishClass",
-                MaxClassSize = 25,
-                ClassLanguage = Enums.Language.English,
-                
-
-            });
-
-            myContext.Class.Add(new Class()
-            {
-                Name = "FrenchClass",
-                MaxClassSize = 30,
-                ClassLanguage = Enums.Language.French
-
-            });*/
 
 
+            /* myContext.Student.Add(new Student()
+             {
+                 StudentName = "Allan",
+                 StudentAge = 20,
+                 ClassList=new List<Class>()
+                 {
+                     new Class()
+                     {
+                         Name="EnglishClass",
+                         MaxClassSize=25,
+                         ClassLanguage=Enums.Language.English
+                     },
+
+                     new Class()
+                     {
+                         Name = "FrenchClass",
+                         MaxClassSize = 30,
+                         ClassLanguage = Enums.Language.French
+                     }
+                 }
 
 
-            myContext.Student.Add(new Student()
-            {
-                StudentName = "Allan",
-                StudentAge = 20,
-                ClassList=new List<Class>()
-                {
-                    new Class()
-                    {
-                        Name="EnglishClass",
-                        MaxClassSize=25,
-                        ClassLanguage=Enums.Language.English
-                    },
+                 });
 
-                    new Class()
-                    {
-                        Name = "FrenchClass",
-                        MaxClassSize = 30,
-                        ClassLanguage = Enums.Language.French
-                    }
-                }
+             myContext.Student.Add(new Student()
+             {
+                 StudentName = "Adrian",
+                 StudentAge = 18,
+                 ClassList = new List<Class>()
+                 {
+                     new Class()
+                     {
+                         Name="SpanishClass",
+                         MaxClassSize=30,
+                         ClassLanguage=Enums.Language.Spanish
+                     },
 
+                     new Class()
+                     {
+                         Name = "RussianClass",
+                         MaxClassSize = 20,
+                         ClassLanguage = Enums.Language.Russian
+                     },
 
-                });
-
-            myContext.Student.Add(new Student()
-            {
-                StudentName = "Adrian",
-                StudentAge = 18,
-                ClassList = new List<Class>()
-                {
-                    new Class()
-                    {
-                        Name="SpanishClass",
-                        MaxClassSize=30,
-                        ClassLanguage=Enums.Language.Spanish
-                    },
-
-                    new Class()
-                    {
-                        Name = "RussianClass",
-                        MaxClassSize = 20,
-                        ClassLanguage = Enums.Language.Russian
-                    },
-
-                    new Class()
-                    {
-                        Name="EnglishClass",
-                        MaxClassSize=25,
-                        ClassLanguage=Enums.Language.English
-                    }
-                }
+                     new Class()
+                     {
+                         Name="EnglishClass",
+                         MaxClassSize=25,
+                         ClassLanguage=Enums.Language.English
+                     }
+                 }
 
 
-            });
+             });*/
 
+            var myClass = myContext.Class.FirstOrDefault(x => x.MaxClassSize > 20);
+            myClass.Name = "change Name";
+            myClass.MaxClassSize = 50;
 
             myContext.SaveChanges();
         }
